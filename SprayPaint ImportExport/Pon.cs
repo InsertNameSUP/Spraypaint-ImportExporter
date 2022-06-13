304adc0bf766ee0d17cedf4cfd375369a628c193
InsertNameSUP <Insert.NameSUP@gmail.com>
2022-06-13 15:34:23 +1000

    Memory Throttle Fixed. Introduce Threading to Encoding and use String Builder with a set capacity to prevent GC freezing

--- SprayPaint ImportExport/Pon.cs
+++ SprayPaint ImportExport/Pon.cs
@@ -1,35 +1,48 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SprayPaint_ImportExport
 {
     class Pon
     {
+        private readonly static object padlock = new object();
+        static Dictionary<int, int> allValues = new Dictionary<int, int>();
         public static Dictionary<int, int> Decode(string decode)
         {
             decode = decode.Substring(1, decode.Length - 3);
             string[] splitValues = decode.Split(";");
-            Dictionary<int, int> allValues = new Dictionary<int, int>();
+            allValues.Clear();
             for (int x = 0; x < splitValues.Length; x += 2)
             {
                 allValues.Add(DecodeHex(splitValues[x]), DecodeHex(splitValues[x + 1]));
+    
             }
             return allValues;
         }
 
         public static string Encode(Dictionary<int, int> pixels)
         {
-            string encodedVal = "[";
-            foreach(KeyValuePair<int, int> value in pixels)
+            ConcurrentBag<string> encodedPixels = new ConcurrentBag<string>();
+            
+            StringBuilder encodedVal = new StringBuilder("[", 585456); // String builder is important for memory allocation. Using a regular string would cause Garbage Collection to
+                                                                       // freeze the program as it tries to keep up with the appending. (585456 was used from the character count of a full grafitti frame.)
+                                                                       // Also String Builder Append method is syncronised and therefore assists in performance during multi-threading.
+           pixels.AsParallel().ForAll(entry =>
             {
-                encodedVal += EncodeHex(value.Key);
-                encodedVal += EncodeHex(value.Value);
-            }
-            encodedVal += "}";
-            return encodedVal;
+                string appendMe = EncodeHex(entry.Key) + EncodeHex(entry.Value);
+                lock (padlock) // Avoid string being written over twice by the different threads.
+                {
+                    encodedVal.Append(appendMe);
+                }
+                //encodedPixels.Add(EncodeHex(entry.Key) + EncodeHex(entry.Value));
+            });
+            encodedVal.Append("}");
+            return encodedVal.ToString();
         }
         private static int DecodeHex(string str)
         {
@@ -37,7 +50,7 @@ namespace SprayPaint_ImportExport
             int num = int.Parse(str, System.Globalization.NumberStyles.HexNumber);
             return num;
         }
-        private static string EncodeHex(int num)
+        public static string EncodeHex(int num)
         {
             string parsedNum = Convert.ToString(num, 16);
             string encodedVal = "X";
